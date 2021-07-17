6915f17f392378627931bba504ff65ecc3c855db
Shota Yamazaki <sysy5867@gmail.com>
2021-07-17 13:14:39 +0900

    tweek week3

--- part1/week3/quicksort.py
+++ part1/week3/quicksort.py
@@ -1,14 +1,38 @@
 import gdown
 import sys
 
-url = ''
-output = 'file.txt'
-gdown.download(url, output, quiet=False)
-
-with open(output) as f:
-    numbers = [int(s.strip()) for s in f.readlines()]
-
 sys.setrecursionlimit(100_000_000)
+
+
+def swap(arr, a, b):
+    arr[a], arr[b] = arr[b], arr[a]
+
+
+def pivot1(arr, l):
+    return arr[l]
+
+
+def pivot2(arr, l, r):
+    # pivotを常に先頭に持ってくるためにswapする
+    swap(arr, l, r)
+    return pivot1(arr, l)
+
+
+def pivot3(arr, l, r):  # 配列の先頭、真ん中、最後の要素の中央値を返す
+    m = l + ((r - l) // 2)
+
+    tmp = [arr[l], arr[m], arr[r]]
+
+    mx = max(tmp)
+    mn = min(tmp)
+
+    tmp.remove(mx)
+    tmp.remove(mn)
+
+    mid_idx = arr.index(tmp[0])
+    swap(arr, l, mid_idx)
+
+    return pivot1(arr, l)
 
 
 def partition(arr, l, r):  # l, r: 各ルーティン内の探索範囲
@@ -26,36 +50,25 @@ def partition(arr, l, r):  # l, r: 各ルーティン内の探索範囲
             arr[j], arr[i] = arr[i], arr[j]
             i += 1
 
-    arr[arr.index(p)], arr[i-1] = arr[i-1], arr[l]
+    swap(arr, arr.index(p), i-1)
 
     m = partition(arr, l, i - 2)
     n = partition(arr, i, r)
 
-    return m + n + (r - l)  # 長さmのsubarrayがあるとすると r - l = m - 1
+    return m + n + (r - l)  # 長さmのsubarrayがあるとすると m - 1 = r - l
 
 
-def pivot1(arr, l):
-    return arr[l]
+def main():
+    url = 'url of input file'
+    output = 'file.txt'
+    gdown.download(url, output, quiet=False)
+
+    with open(output) as f:
+        numbers = [int(s.strip()) for s in f.readlines()]
+
+    ans = partition(numbers, 0, len(numbers) - 1)
+    print(ans)
 
 
-def pivot2(arr, l, r):
-    # pivotを常に先頭に持ってくるためにswapする
-    arr[l], arr[r] = arr[r], arr[l]
-    return pivot1(arr, l)
-
-
-def pivot3(arr, l, r):
-    m = l + ((r - l) // 2)
-
-    tmp = [arr[l], arr[m], arr[r]]
-
-    mx = max(tmp)
-    mn = min(tmp)
-
-    tmp.remove(mx)
-    tmp.remove(mn)
-
-    mid_idx = arr.index(tmp[0])
-    arr[l], arr[mid_idx] = arr[mid_idx], arr[l]
-
-    return pivot1(arr, l)
+if __name__ == '__main__':
+    main()
